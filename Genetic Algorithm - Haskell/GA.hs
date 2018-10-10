c32bc53d421ef2c405237b24c4777283a7b6fe90
HenriqueCA <Henrique.arriel@ccc.ufcg.edu.br>
2018-10-10 15:04:56 -0300

    Minor fixes and organization do pai

--- Genetic Algorithm - Haskell/GA.hs
+++ Genetic Algorithm - Haskell/GA.hs
@@ -3,17 +3,22 @@ import System.IO.Unsafe
 import System.Random
 import Data.List
 
+-- Variaveis iniciais
+populationSize = 40;
+chromossomeSize = mazeSize^2 - numOfWalls maze
 mazeSize = 5
 wallIcon = '#'
+spawn = (3,3)
+exit = (4,1)
 maze = [['#', '#', '#', '#', '#'],
         ['#', ' ', ' ', ' ', 'E'],
         ['#', ' ', '#', '#', '#'],
         ['#', ' ', ' ', 'S', '#'],
         ['#', '#', '#', '#', '#']]
 
-getRandomInteger :: (Int, Int) -> Int
-getRandomInteger (a, b) = unsafePerformIO (randomRIO (a, b))
+data Individuo = Individuo {fitness :: Integer, moves :: [Char]} deriving (Show)
 
+-- Funcoes de validacao.
 isValid :: (Int, Int) -> Bool
 isValid (a, b) = (0 <= a) && (a < mazeSize) && (0 <= b) && (b < mazeSize) 
 
@@ -23,6 +28,14 @@ isAWall (a, b) = (maze !! a) !! b == wallIcon
 isExit :: (Int, Int) -> Bool
 isExit (a, b) = isValid (a, b) && (maze !! a) !! b == 'E'
 
+isValidMove :: (Int, Int) -> Bool
+isValidMove (x, y) = isValid (x, y) && not (isAWall (x, y))
+
+-- Funcoes de movimento
+getRandomInteger :: (Integer, Integer) -> Int
+--getRandomInteger (a, b) = unsafePerformIO (randomRIO (a, b))
+getRandomInteger (a, b) = 3
+
 sumVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
 sumVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
@@ -30,18 +43,6 @@ makeAMove :: (Int, Int) -> Char -> (Int, Int)
 makeAMove (x, y) m =
     let move = getMove m
     in sumVectors (x, y) move
-
-isValidMove :: (Int, Int) -> Bool
-isValidMove (x, y) = isValid (x, y) && not (isAWall (x, y))
-
-numOfWalls :: [[Char]] -> Int
---numOfWalls maze = sum [[1 | icon <- line, isAWall icon ]| line <- maze]
-numOfWalls xxs = sum [sum [1 | x <- xs, x == '#' ]| xs <- xxs]
-
-getMove 'U' = (-1, 0)
-getMove 'D' = (1, 0)
-getMove 'L' = (0, -1)
-getMove 'R' = (0, 1)
 
 randomMove :: Char
 randomMove
@@ -51,45 +52,49 @@ randomMove
     | num == 4 = 'R'
     where
         num = getRandomInteger(1, 4)
-
+    
 randomMoves :: Int -> [Char]
 randomMoves size = [randomMove | _ <- [1..size]]
 
-cromossomeSize = mazeSize^2 - numOfWalls maze
+-- Funcoes auxiliares
+numOfWalls :: [[Char]] -> Int
+numOfWalls xxs = sum [sum [1 | x <- xs, x == wallIcon ]| xs <- xxs]
 
-data Individuo = Individuo {fitness :: Integer, moves :: [Char]} deriving (Show)
+getMove 'U' = (-1, 0)
+getMove 'D' = (1, 0)
+getMove 'L' = (0, -1)
+getMove 'R' = (0, 1)
 
+-- Funcoes de inicializacao
 buildIndividuo :: Individuo
 buildIndividuo = newIndividuo
                 where
                     fitness = 10^6
-                    moves = randomMoves cromossomeSize
+                    moves = randomMoves chromossomeSize
                     newIndividuo = Individuo fitness moves
 
---Considerando uma população de 40 individuos
 initPopulation :: [Individuo]
-initPopulation = [ buildIndividuo | _ <- [1..40]]
+initPopulation = [ buildIndividuo | _ <- [1..populationSize]]
 
+-- Funcoes do algoritmo genetico
 calculateRecursive :: [Char] -> (Int, Int) -> Integer -> [(Int, Int)] -> Integer
 
 calculateRecursive [] _ f _ = f
         
---De acordo com o feito em C++, dá pra simplificar eu acho
 calculateRecursive (m:ms) (x, y) f visited = 
         let newPos = makeAMove (x, y) m
         in if isExit newPos
             then f*10^6
-            else if isValidMove newPos
+            else if isValidMove newPos --Movimento valido
             then if newPos `elem` visited 
-                then calculateRecursive (ms) newPos (f-500) visited
-                else calculateRecursive (ms) newPos (f-200) (newPos:visited)
-            else if newPos `elem` visited 
-                then calculateRecursive (ms) (x, y) (f-700) visited
-                else calculateRecursive (ms) (x, y) (f-400) (newPos:visited)
+                then calculateRecursive (ms) newPos (f-500) visited -- Movimento valido e visitado.
+                else calculateRecursive (ms) newPos (f-200) (newPos:visited) -- Movimento valido e nao visitado.
+            else if newPos `elem` visited --Movimento invalido
+                then calculateRecursive (ms) (x, y) (f-700) visited -- Movimento invalido e visitado.
+                else calculateRecursive (ms) (x, y) (f-400) (newPos:visited) -- Movimento invalido e nao visitado.
 
 calculateFitnessIndividual :: Individuo -> Integer
-calculateFitnessIndividual ind = calculateRecursive (moves ind) (0, 0) (fitness ind) []
+calculateFitnessIndividual ind = calculateRecursive (moves ind) spawn (fitness ind) []
                 
 calculateFitness :: [Individuo] -> [Integer]
 calculateFitness xs = [calculateFitnessIndividual x | x <- xs]
-
