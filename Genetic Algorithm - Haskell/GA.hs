63507118efb0bce57e87902b5001b222003bcf4e
HenriqueCA <Henrique.arriel@ccc.ufcg.edu.br>
2018-10-26 04:01:03 -0300

    dud

--- Genetic Algorithm - Haskell/GA.hs
+++ Genetic Algorithm - Haskell/GA.hs
@@ -1,30 +1,4 @@
-module GA (
-    isValid,
-    isAWall,
-    isExit,
-    isValidMove,
-    getRandomInteger,
-    sumVectors,
-    makeAMove,
-    randomMove,
-    randomList,
-    randomMoves,
-    numOfWalls,
-    coherentMoves,
-    buildIndividuo,
-    initPopulation,
-    drawMaze,
-    drawFittest,
-    drawFittestAux,
-    clearScreen,
-    mutation,
-    moveAppend,
-    moveFlip,
-    moveFlipAux,
-    calculateRecursive,
-    calculateFitnessIndividual,
-    calculateFitness
-) where 
+module GA where 
 
 -- Generate a random number given a range.
 import System.Process as SP
@@ -33,28 +7,26 @@ import System.Random
 import Data.List
 
 -- Variaveis iniciais
-populationSize;
-chromossomeSize = mazeSize^2 - numOfWalls maze
 mutationChance = 40 --Porcentagem
-mazeSize = 5
 wallIcon = '#'
-spawn = (3,3)
-exit = (4,1)
-maze = [['#', '#', '#', '#', '#'],
-        ['#', ' ', ' ', ' ', 'E'],
-        ['#', ' ', '#', '#', '#'],
-        ['#', ' ', ' ', 'S', '#'],
-        ['#', '#', '#', '#', '#']]
+populationSize = 1000
+--spawn = (3,3)
+--exit = (4,1)
+--maze = [['#', '#', '#', '#', '#'],
+--        ['#', ' ', ' ', ' ', 'E'],
+--        ['#', ' ', '#', '#', '#'],
+--        ['#', ' ', ' ', 'S', '#'],
+--        ['#', '#', '#', '#', '#']]
 
-mazeDists = [[-1,-1,-1,-1,-1],
-             [-1,3,2,1,0],
-             [-1,4,-1,-1,-1],
-             [-1,5,6,7,-1],
-             [-1,-1,-1,-1,-1]]
+--mazeDists = [[-1,-1,-1,-1,-1],
+--             [-1,3,2,1,0],
+--             [-1,4,-1,-1,-1],
+--            [-1,5,6,7,-1],
+--             [-1,-1,-1,-1,-1]]
 
 --population = initPopulation
 
-data Individuo = Individuo {fitness :: Integer, moves :: [Char]} deriving (Show)
+data Individuo = Individuo {fitness :: Int, moves :: [Char]} deriving (Show)
 
 instance Eq Individuo where
     (i1) == (i2) = fitness i1 == fitness i2
@@ -63,17 +35,17 @@ instance Ord Individuo where
     (i1) `compare` (i2) = fitness i2 `compare` fitness i1
 
 -- Funcoes de validacao.
-isValid :: (Int, Int) -> Bool
-isValid (a, b) = (0 <= a) && (a < mazeSize) && (0 <= b) && (b < mazeSize) 
+isValid :: (Int, Int) -> [[Char]] -> Bool
+isValid (a, b) m = (0 <= a) && (a < length m) && (0 <= b) && (b < length m) 
 
-isAWall :: (Int, Int) -> Bool
-isAWall (a, b) = (maze !! a) !! b == wallIcon
+isAWall :: (Int, Int) -> [[Char]] -> Bool
+isAWall (a, b) m = (m !! a) !! b == wallIcon
 
-isExit :: (Int, Int) -> Bool
-isExit (a, b) = isValid (a, b) && (maze !! a) !! b == 'E'
+isExit :: (Int, Int) -> [[Char]] -> Bool
+isExit (a, b) m = (isValid (a, b) m) && (m !! a) !! b == 'E'
 
-isValidMove :: (Int, Int) -> Bool
-isValidMove (x, y) = isValid (x, y) && not (isAWall (x, y))
+isValidMove :: (Int, Int) -> [[Char]] -> Bool
+isValidMove (x, y) m = (isValid (x, y) m) && not (isAWall (x, y) m)
 
 -- Funcoes de movimento
 getRandomInteger :: (Int,Int) -> Int
@@ -117,19 +89,20 @@ getMove 'L' = (0, -1)
 getMove 'R' = (0, 1)
 
 -- Funcoes de inicializacao
-buildIndividuo :: Individuo
-buildIndividuo = newIndividuo
+buildIndividuo :: Int -> Individuo
+buildIndividuo chromossomeSize = newIndividuo
                 where
                     fitness = 10^6
                     moves = randomMoves chromossomeSize
                     newIndividuo = Individuo fitness moves
 
-initPopulation :: [Individuo]
-initPopulation = [ buildIndividuo | _ <- [1..populationSize]]
+initPopulation :: Int -> [Individuo]
+initPopulation chromossomeSize = [ (buildIndividuo chromossomeSize) | _ <- [1..populationSize]]
 
 -- Funcoes de impressao
-drawMaze :: [[Char]] -> IO ()
-drawMaze = putStrLn . unlines 
+drawMaze :: [[Char]] -> [Char]
+drawMaze [] = ""
+drawMaze (x:xs) = x ++ "\n" ++ (drawMaze xs)
 
 drawFittest :: [[Char]] -> (Int,Int) -> Int -> [[Char]]
 drawFittest maze (x,y) size = [[drawFittestAux maze (x,y) (j,i) | i <- [0..size-1]] | j <- [0..size-1]]
@@ -139,10 +112,6 @@ drawFittestAux maze (x,y) (i,j)
     |x == i && y == j = '*'
     |x /= i || y /= j = (maze !! i) !! j
 
-clearScreen :: IO ()
-clearScreen = do
-    SP.system "clear"
-    return ()
 
 -- Funcoes do algoritmo genetico
 mutation :: [Char] -> [Char]
@@ -165,25 +134,26 @@ moveFlipAux individual x target
         | x == target = coherentMoves (individual !! x)
         | x /= target = individual !! x
 
-calculateRecursive :: [Char] -> (Int, Int) -> Integer -> [(Int, Int)] -> Integer
-calculateRecursive [] _ f _ = f
-calculateRecursive (m:ms) (x, y) f visited = 
-        let newPos = makeAMove (x, y) m
-        in if isExit newPos
+calculateRecursive :: [Char] -> (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> Int -> [(Int, Int)] -> Int
+calculateRecursive [] ((maze, mazeDists), (position,exit)) f visited = 
+    f - (((mazeDists !! (fst position)) !! (snd position)) * 1000)
+calculateRecursive (m:ms) ((maze, mazeDists), (position,exit)) f visited = 
+        let newPos = makeAMove position m
+        in if isExit newPos maze
             then f*10^6
-            else if isValidMove newPos --Movimento valido
+            else if isValidMove newPos maze--Movimento valido
             then if newPos `elem` visited 
-                then calculateRecursive (ms) newPos (f-500) visited -- Movimento valido e visitado.
-                else calculateRecursive (ms) newPos (f-200) (newPos:visited) -- Movimento valido e nao visitado.
+                then calculateRecursive (ms) ((maze,mazeDists), (newPos,exit)) (f-500) visited -- Movimento valido e visitado.
+                else calculateRecursive (ms) ((maze,mazeDists), (newPos,exit)) (f-200) (newPos:visited) -- Movimento valido e nao visitado.
             else if newPos `elem` visited --Movimento invalido
-                then calculateRecursive (ms) (x, y) (f-700) visited -- Movimento invalido e visitado.
-                else calculateRecursive (ms) (x, y) (f-400) (newPos:visited) -- Movimento invalido e nao visitado.
+                then calculateRecursive (ms) ((maze,mazeDists), (position,exit)) (f-700) visited -- Movimento invalido e visitado.
+                else calculateRecursive (ms) ((maze,mazeDists), (position,exit)) (f-400) (newPos:visited) -- Movimento invalido e nao visitado.
 
-calculateFitnessIndividual :: Individuo -> Integer
-calculateFitnessIndividual ind = calculateRecursive (moves ind) spawn (fitness ind) []
+calculateFitnessIndividual :: Individuo -> (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> Int
+calculateFitnessIndividual ind tuple = calculateRecursive (moves ind) tuple (fitness ind) []
                 
-calculateFitness :: [Individuo] -> [Integer]
-calculateFitness xs = [calculateFitnessIndividual x | x <- xs]
+calculateFitness :: [Individuo] -> (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> [Individuo]
+calculateFitness xs tuple = [Individuo (calculateFitnessIndividual x tuple) (moves x) | x <- xs]
 
 groupsChance = [0.5, 0.25, 0.15, 0.08 , 0.02]
 groupsArray = initGroupsChance
@@ -202,16 +172,17 @@ initGroupsChance' [] groupsArray _ = groupsArray
 quartil = [round $ fromIntegral(populationSize) * 0.01, round $ fromIntegral(populationSize) * 0.25, round $ fromIntegral(populationSize) * 0.4, round $ fromIntegral(populationSize) * 0.75]
 groups = [(0, quartil !! 0), ((quartil !! 0) + 1, quartil !! 1), ((quartil !! 1) + 1, quartil !! 2), ((quartil !! 2) + 1, quartil !! 3), ((quartil !! 3) + 1, quartil !! 4)]
 
-crossover :: [Individuo] -> [String] -> Integer -> [Individuo]
-crossover newPopulation chromossomeSet n
+crossover :: [Individuo] -> [Individuo] -> [String] -> Int -> Int -> [Individuo]
+crossover population newPopulation chromossomeSet n chromossomeSize
     | n < populationSize =
-        let newMoves = crossoverIndividuo
+        let newMoves = crossoverIndividuo population chromossomeSize
         in if newMoves `elem` chromossomeSet
-            then crossover newPopulation chromossomeSet n 
-            else crossover ((Individuo 0 newMoves):newPopulation) (newMoves:chromossomeSet) (n+1)
+            then crossover population newPopulation chromossomeSet n chromossomeSize 
+            else crossover population ((Individuo (10^6) (mutation newMoves)):newPopulation) (newMoves:chromossomeSet) (n+1) chromossomeSize
     | otherwise = newPopulation
 
-crossoverIndividuo =
+crossoverIndividuo :: [Individuo] -> Int -> String
+crossoverIndividuo population chromossomeSize =
     let pairL = groups !! ((groupsArray !! getRandomInteger(0, 99)) - 1)
         pairR = groups !! ((groupsArray !! getRandomInteger(0, 99)) - 1)
         l1 = fst pairL
@@ -221,9 +192,26 @@ crossoverIndividuo =
         daddy = moves $ population !! getRandomInteger(l1, r1)
         mommy = moves $ population !! getRandomInteger(l2, r2)
         crossoverPoint = getRandomInteger(1, chromossomeSize)
-        halfSon = crossoverMommy 0 crossoverPoint mommy ""
-    in crossoverDaddy ((length halfSon) - 1) chromossomeSize daddy halfSon
+        --halfSon = crossoverMommy 0 crossoverPoint mommy ""
+    in -- crossoverDaddy ((length halfSon) - 1) chromossomeSize daddy halfSon
+        crossoverParents crossoverPoint mommy daddy chromossomeSize
     --completedSon
+
+
+crossoverParents crossoverPoint mommy daddy chromossomeSize = 
+    let pointLessThanMommy = crossoverPoint < length mommy
+        pointLessThanDaddy = length daddy <= chromossomeSize    
+    in case (pointLessThanMommy, pointLessThanDaddy) of
+        (True, True) ->  [mommy !! x | x <- [0..(crossoverPoint - 1)]] ++ [if y == crossoverPoint then coherentMoves $ mommy !! (crossoverPoint - 1) else daddy !! y | y <- [crossoverPoint..((length daddy) - 1)]]
+        (True, False) -> [mommy !! x | x <- [0..(crossoverPoint - 1)]] ++ [if y == crossoverPoint then coherentMoves $  mommy !! (crossoverPoint - 1) else daddy !! y | y <- [crossoverPoint..(chromossomeSize-1)]]
+        (False, True) -> [mommy !! x | x <- [0..(length mommy - 1)]] ++ [if y == crossoverPoint then coherentMoves $ (last mommy) else daddy !! y | y <- [crossoverPoint..(length daddy - 1)]]
+        (False, False) -> [mommy !! x | x <- [0..(length mommy - 1)]] ++ [if y == crossoverPoint then coherentMoves $  mommy !! (crossoverPoint - 1) else daddy !! y | y <- [crossoverPoint..(chromossomeSize-1)]]
+
+{-
+crossoverMommy crossoverPoint mommy
+    | crossoverPoint < length mommy = [mommy !! x | x <- [0..crossoverPoint]]
+    | otherwise = [mommy !! x | x <- [0..(length mommy - 1)]]
+
 
 crossoverMommy :: Int -> Int -> String -> String -> String
 crossoverMommy _ _ [] son = son
@@ -239,3 +227,4 @@ crossoverDaddy n crossoverPoint (move:daddy) son
     | n == crossoverPoint = crossoverDaddy (n+1) crossoverPoint daddy (son ++ [coherentMoves $ last son])
     | n < chromossomeSize = crossoverDaddy (n+1) crossoverPoint daddy (son ++ [move])
     | otherwise = son
+-}
--- Genetic Algorithm - Haskell/Main.hs
+++ Genetic Algorithm - Haskell/Main.hs
@@ -2,6 +2,7 @@ import GA
 import System.Process as SP
 import System.IO.Unsafe
 import Control.Concurrent
+import Data.List
 
 
 display = do
@@ -14,14 +15,6 @@ display = do
     putStrLn "|__|  |__| /__/     \\__\\ /________||_______|   |_______/     \\______/  |_______|    \\__/     |_______|| _| `._____|"
     putStrLn "\n#####################################################################################################################\n"
 
-    
-maze = [['#', '#', '#', '#', '#'],
-    ['#', ' ', ' ', ' ', 'E'],
-    ['#', ' ', '#', '#', '#'],
-    ['#', ' ', ' ', 'S', '#'],
-    ['#', '#', '#', '#', '#']]
-
-mazeSize = 5
 
 clearScreen :: IO ()
 clearScreen = do
@@ -33,82 +26,185 @@ main = do
     putStr "Size of maze: "
     input <- readLn :: IO Int
     let mazeSize = adjustMazeSize input
+
+    let mazeTuple = unsafePerformIO (generateMaze mazeSize) 
+    let maze = getMaze mazeTuple
+    let mazeDist = getmazeDists mazeTuple
+    let spawn = getSpawn mazeTuple
+
+    let chromossomeSize = (mazeSize^2) - numOfWalls maze
     
-    -- generateMaze terá que retornar uma lista contendo: o labirinto, spawn e exit.
-    let maze = generateMaze mazeSize
-    --let spawn = findSpawn maze
-    --let exit = findExit maze 
-    
-    putStrLn "Size of Population: "
-    populationSize <- readLn :: IO Int
+    --putStrLn "Size of Population: "
+    --let populationSize <- readLn :: IO Int
 
-    let tuple = generations 0 initPopulation
-    let lastGeneration = fst tuple
-    let bestIndivid = snd tuple
-    -- Mostra solução do GA.
-    showSolution (moves bestIndivid) spawn maze
+    generations 0 (initPopulation chromossomeSize) mazeTuple chromossomeSize
 
-    let dumbTuple = dumbGeneration 0 initPopulation
-    let dumbGeneration = fst dumbTuple;
-    let dumbIndivid = snd dumbTuple;
-    --Mostra solução do individuo burro.
-    showSolution (moves dumbIndivid) spawn maze
+    threadDelay 3000000
 
--- GERAÇÃO BURRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA QUE NEM TUA MÃE KKKKKKK
-dumbGeneration :: Int -> [Individuo] -> (Int, Individuo)
-dumbGeneration 1000 population = (1000, head population)
-dumbGeneration gen population = do
-    let sortedNewPopulation = sort (calculateFitness population)
+    dumbGeneration 0 (initPopulation chromossomeSize) mazeTuple chromossomeSize
+
+
+-- GERAÇÃO BURRAAAAA
+dumbGeneration :: Int -> [Individuo] -> (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> Int -> IO()
+dumbGeneration 100 population mazeTuple _ = showSolution (moves (head population)) (getSpawn mazeTuple) (getMaze mazeTuple)
+dumbGeneration gen population mazeTuple chromossomeSize = do
+    putStr "And now watch the dumb algorithm"
+    threadDelay 3000000
+    clearScreen
+    let sortedNewPopulation = sort (calculateFitness population mazeTuple)
     putStr (show gen ++ " " ++ unlines [moves (head sortedNewPopulation)])
     let finished = checkFinished sortedNewPopulation
     if finished
-        then return (gen, head sortedNewPopulation)
-        else dumbGeneration (gen +1) initPopulation
+        then showSolution (moves (head sortedNewPopulation)) (getSpawn mazeTuple) (getMaze mazeTuple)
+        else dumbGeneration (gen+1) (initPopulation chromossomeSize) mazeTuple chromossomeSize
  
 -- Vai printando os passos do melhor individuo
 showSolution :: [Char] -> (Int,Int) -> [[Char]] -> IO()
-showSolution [] (x,y) maze = print (drawFittest maze (x,y) mazeSize)
-showSolution [mv:steps] (x,y) maze = do
-    print (drawFittest maze (x,y) mazeSize)
+showSolution [] (x,y) maze = putStrLn (drawMaze $ drawFittest maze (x,y) (length maze))
+showSolution (mv:steps) (x,y) maze = do
+    putStrLn (drawMaze $ drawFittest maze (x,y) (length maze))
     let newPosition = makeAMove (x,y) mv
-    in threadDelay 300000 -- Dormir por 0.3 segundos porque recebe o tempo em microsegundos.
-       clearScreen
-       showSolution steps newPosition maze
+    threadDelay 300000
+    clearScreen
+    if isValidMove newPosition maze
+        then showSolution steps newPosition maze
+        else showSolution steps (x,y) maze 
 
 
-limitOfGenerations = 1000
+getExit :: (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> (Int,Int)
+getExit mazeTuple = snd (snd mazeTuple)
 
-generations :: Int -> [Individuo] -> (Int, Individuo)
-generations 1000 population = (1000,head population)
-generations gen population = do
-    let sortedNewPopulation = sort (calculateFitness population)
+getSpawn :: (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> (Int,Int)
+getSpawn mazeTuple = fst (snd mazeTuple)
+
+getmazeDists :: (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> [[Int]]
+getmazeDists mazeTuple = snd (fst mazeTuple)
+
+getMaze :: (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> [[Char]] 
+getMaze mazeTuple = fst (fst mazeTuple)
+
+generations :: Int -> [Individuo] -> (([[Char]], [[Int]]),((Int, Int), (Int, Int))) -> Int -> IO()
+generations 100 population mazeTuple _ = showSolution (moves (head population)) (getSpawn mazeTuple) (getMaze mazeTuple)
+generations gen population mazeTuple chromossomeSize = do
+    let sortedNewPopulation = sort (calculateFitness population mazeTuple)
     putStr (show gen ++ " " ++ unlines [moves $ head sortedNewPopulation])
     let finished = checkFinished sortedNewPopulation
     if finished
-        then return (gen,head sortedNewPopulation)
-        else generations (gen + 1) (crossover (sortedNewPopulation))
+        then showSolution (moves (head sortedNewPopulation)) (getSpawn mazeTuple) (getMaze mazeTuple)
+        else generations (gen + 1) (crossover sortedNewPopulation [] [] 0 chromossomeSize) mazeTuple chromossomeSize
 
 
 checkFinished :: [Individuo] -> Bool
 checkFinished [] = False
 checkFinished (ind:inds) 
-    | getFitness ind > 10^8 = True
+    | (fitness ind) > (10^8) = True
     | otherwise = False
 
+                          
+
 -- Gera e printa maze
--- Precisa receber o tamanho do labirinto
-generateMaze :: Int -> [[Char]]
+generateMaze :: Int -> IO (([[Char]], [[Int]]),((Int, Int), (Int, Int)))
 generateMaze mazeSize = do
-        --initVariables
-        --let maze = mazeGenerator mazeSize retorna o labirinto.
-        drawMaze maze
+        let tuple = mazeGenerator mazeSize
+        let maze = getMaze tuple
+        let mazeDist = getmazeDists tuple
+        let spawn = getSpawn tuple
+        let exit = getExit tuple
+
+        putStrLn (drawMaze maze)
         putStrLn "Generate new maze?(y/n): "
         resposta <- getLine
         if resposta == "y"
             then generateMaze mazeSize
-            else return maze
+            else return ((maze,mazeDist),(spawn,exit)) :: IO (([[Char]], [[Int]]),((Int, Int), (Int, Int)))
     
 adjustMazeSize :: Int -> Int
 adjustMazeSize num | (num <= 2) = 3
                 | (num `mod` 2) == 0 = num + 1
                 | otherwise = num
+
+--Apenas uma implementacao do operador ternário
+ter :: Int -> a -> a -> a
+ter i a b = if (i == 0) then b else a 
+
+--Operacoes em matrizes e listas
+generateMatrix :: Int -> Int -> t -> [[t]]
+generateMatrix n m c = replicate n (replicate m c)
+
+--Troca um elemento da matriz m: m[i][j] = c e retorna a nova matriz
+changeMatrix :: Int -> Int -> t -> [[t]] -> [[t]]
+changeMatrix i j c m
+    | i == 0 = [changeList j c (head m)] ++ tail m
+    | otherwise = [head m] ++ (changeMatrix (i-1) j c (tail m))
+
+--Troca um elemento da lista l: l[i] = c e retorna a nova lista
+changeList :: Int -> t -> [t] -> [t]
+changeList i c l
+    | i == 0 = [c] ++ tail l
+    | otherwise = [head l] ++ changeList (i-1) c (tail l)
+
+-- Funcoes de aleatoriedade
+
+randomRange :: Int -> Int -> Int -> Int
+randomRange start end step = 
+    (((getRandomInteger(1,2000000000) `mod` (end - start + 1)) `quot` step) * step) + start
+
+--Gera uma ordem aleatoria para uma sequencia de tamanho n
+genShuffledSequence :: Int -> [Int]
+genShuffledSequence n = genShuffledSequenceRecursion [0..n-1]
+
+genShuffledSequenceRecursion :: [Int] -> [Int]
+genShuffledSequenceRecursion l = 
+    if (l == []) then []
+    else let n = getRandomInteger(0, (length l)-1) in
+        let newL = changeList n (head l) l in [l !! n] ++ genShuffledSequenceRecursion (tail newL)
+
+--Gera um labirinto aleatório de tamanho n x n
+--Retorna: Matriz maze, Matriz dists.
+mazeGenerator :: Int -> (([[Char]], [[Int]]),((Int, Int), (Int, Int)))
+mazeGenerator n =
+    let x = randomRange 1 (n-1) 2
+        y = getRandomInteger(0, 1) * (n-1)
+        s = getRandomInteger(0, 1)
+    in let
+        coords = ((ter s x y, ter s y x), (ter s x (ter y (y-1) 1), ter s (ter y (y-1) 1) x))
+        spawn = (randomRange 1 (n-1) 2, randomRange 1 (n-1) 2)
+    in let maze = changeMatrix (fst (snd coords)) (snd (snd coords)) ' ' (changeMatrix (fst (fst coords)) (snd (fst coords)) 'E' (generateMatrix n n '#'))
+    in let recur = generateMazeRecursion (fst (snd coords)) (snd (snd coords)) maze (generateMatrix n n 0) 1
+    --in (([['A']], [[0]]), ((1, 1), (1, 1)))
+    in ((changeMatrix (fst spawn) (snd spawn) 'S' (fst recur), (snd recur)), (spawn, fst coords))
+
+generateMazeRecursion :: Int -> Int -> [[Char]] -> [[Int]] -> Int -> ([[Char]], [[Int]])
+generateMazeRecursion x y m d dist = 
+    let d0 = changeMatrix x y dist d
+        moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        ordem = genShuffledSequence 4
+        p1x = (x + 2 * (fst (moves !! (ordem !! 0))), x + (fst (moves !! (ordem !! 0))))
+        p1y = (y + 2 * (snd (moves !! (ordem !! 0))), y + (snd (moves !! (ordem !! 0))))
+        p2x = (x + 2 * (fst (moves !! (ordem !! 1))), x + (fst (moves !! (ordem !! 1))))
+        p2y = (y + 2 * (snd (moves !! (ordem !! 1))), y + (snd (moves !! (ordem !! 1))))
+        p3x = (x + 2 * (fst (moves !! (ordem !! 2))), x + (fst (moves !! (ordem !! 2))))
+        p3y = (y + 2 * (snd (moves !! (ordem !! 2))), y + (snd (moves !! (ordem !! 2))))
+        p4x = (x + 2 * (fst (moves !! (ordem !! 3))), x + (fst (moves !! (ordem !! 3))))
+        p4y = (y + 2 * (snd (moves !! (ordem !! 3))), y + (snd (moves !! (ordem !! 3))))
+    in let v1 = ((isValid (fst p1x, fst p1y) m) && (isAWall (fst p1x, fst p1y) m))
+    in let
+        m1 = if v1 then (changeMatrix (fst p1x) (fst p1y) ' ' (changeMatrix (snd p1x) (snd p1y) ' ' m)) else m
+        d1 = if v1 then (changeMatrix (snd p1x) (snd p1y) (dist+1) d0) else d0
+    in let recur1 = if v1 then generateMazeRecursion (fst p1x) (fst p1y) m1 d1 (dist+2) else (m1, d1)
+    in let v2 = ((isValid (fst p2x, fst p2y) (fst recur1)) && (isAWall (fst p2x, fst p2y) (fst recur1)))
+    in let
+        m2 = if v2 then (changeMatrix (fst p2x) (fst p2y) ' ' (changeMatrix (snd p2x) (snd p2y) ' ' (fst recur1))) else fst recur1
+        d2 = if v2 then (changeMatrix (snd p2x) (snd p2y) (dist+1) (snd recur1)) else (snd recur1)
+        r1 = if v2 then (r1+2) else r1
+    in let recur2 = if v2 then generateMazeRecursion (fst p2x) (fst p2y) m2 d2 (dist+2) else (m2, d2)
+    in let v3 = ((isValid (fst p3x, fst p3y) (fst recur2)) && (isAWall (fst p3x, fst p3y) (fst recur2)))
+    in let
+        m3 = if v3 then (changeMatrix (fst p3x) (fst p3y) ' ' (changeMatrix (snd p3x) (snd p3y) ' ' (fst recur2))) else fst recur2
+        d3 = if v3 then (changeMatrix (snd p3x) (snd p3y) (dist+1) (snd recur2)) else (snd recur2)
+    in let recur3 = if v3 then generateMazeRecursion (fst p3x) (fst p3y) m3 d3 (dist+2) else (m3, d3)
+    in let v4 = ((isValid (fst p4x, fst p4y) (fst recur3)) && (isAWall (fst p4x, fst p4y) (fst recur3)))
+    in let
+        m4 = if v4 then (changeMatrix (fst p4x) (fst p4y) ' ' (changeMatrix (snd p4x) (snd p4y) ' ' (fst recur3))) else fst recur3
+        d4 = if v4 then (changeMatrix (snd p4x) (snd p4y) (dist+1) (snd recur3)) else (snd recur3)
+    in if v4 then generateMazeRecursion (fst p4x) (fst p4y) m4 d4 (dist+2) else (m4, d4)
